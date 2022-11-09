c80bc43eb8d954010879b3c2b15a80ff33379e1c
VladislavNetbayDev <netbay.vladislav@gmail.com>
2022-11-09 11:46:32 +0300

    commit all files

---
+++ ProgramLanguages/Examples/Examples006/Program.cs
@@ -0,0 +1,18 @@
+﻿// 6. Выяснить является ли число чётным
+int number = 4;
+if (number % 2 ==0) Console.WriteLine("even");
+else Console.WriteLine("odd");
+Console.Write("end");
+
+
+
+
+// Console.WriteLine("Введите число");
+// string number = Console.ReadLine();
+// if (Convert.ToInt32(number) % 2 ==0) Console.WriteLine("even");
+// else Console.WriteLine("odd");
+
+
+
+
+
---
+++ ProgramLanguages/Seminar2/Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// 20 задача 20:00
+void result (int a){
+    int b = a / 100 % 10;
+    if (a > 99) Console.WriteLine(b);
+    else Console.WriteLine("Третьей цифры нет");
+}
+result (12);
+
+// // Написать метод, который выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+// int [] array = {7, 8};
+// int find = array[2];
+// int index = 0;
+// int n = array.Length;
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(array[index]);
+//         break;
+//     }
+//     index = index + 1;
+// }
