a9994a8739127ac8f8bb8e561818fce79aa570e9
VladislavNetbayDev <netbay.vladislav@gmail.com>
2022-11-30 11:15:13 +0300

    Сделал задание на вывод куба числа

---
+++ ProgramLanguages/Seminar1/Examples/Task004/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+string inputValue = Console.ReadLine();
+int value = Convert.ToInt32(inputValue);
+
+int squareValue = value * value * value;
+System.Console.WriteLine($"Куб числа равен {value} равен {squareValue}");
+
