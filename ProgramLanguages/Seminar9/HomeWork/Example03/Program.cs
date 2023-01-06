1565d093f7d360a873aa0c0e9e0cefcb604eeadb
VladislavNetbayDev <netbay.vladislav@gmail.com>
2023-01-06 21:06:09 +0300

    Решил Example03

---
+++ ProgramLanguages/Seminar9/HomeWork/Example03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int InputValue(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int Ackerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Ackerman(n - 1, 1);
+    else
+      return Ackerman(n - 1, Ackerman(n, m - 1));
+}
+
+int N = InputValue("Введите число N: ");
+int M = InputValue("Введите число M: ");
+System.Console.WriteLine(Ackerman(N, M));
