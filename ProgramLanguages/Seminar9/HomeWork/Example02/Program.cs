e349d7584a0ce246da9ca821e142337d550a461b
VladislavNetbayDev <netbay.vladislav@gmail.com>
2023-01-06 20:51:12 +0300

    Решил Example01 и Example02

---
+++ ProgramLanguages/Seminar9/HomeWork/Example02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. +
+// Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputValue(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int SumRecursia(int N, int M)
+{
+    if (N < M) return 0;
+    else return N + SumRecursia(N - 1, M);
+}
+
+int N = InputValue("Введите число N: ");
+int M = InputValue("Введите число M: ");
+System.Console.WriteLine(SumRecursia(N, M));
+ 
