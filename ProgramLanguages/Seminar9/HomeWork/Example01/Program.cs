133f0a85b3cdeabcd7a3fc1cf300c22046d2bdc8
VladislavNetbayDev <netbay.vladislav@gmail.com>
2023-01-06 20:52:00 +0300

    Решил Example01 и Example02

---
+++ ProgramLanguages/Seminar9/HomeWork/Example01/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Задайте значения M и N. 
+//  Напишите программу, 
+//  которая выведет все чётные натуральные числа 
+//  в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int InputValue(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintNumbers(int N, int M)
+{
+    if(N < M)
+    {
+        return;
+    }
+    PrintNumbers(N - 1, M);
+    if (N % 2 ==0) System.Console.Write($"{N}; ");
+}
+
+int N = InputValue("Введите число N: ");
+int M = InputValue("введите число M: ");
+PrintNumbers(N, M);
