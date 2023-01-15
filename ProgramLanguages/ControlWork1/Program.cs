29d1f77773eed67b11cfb9d3570057c9813514aa
VladislavNetbayDev <netbay.vladislav@gmail.com>
2023-01-15 14:11:42 +0300

    Задал функции

--- ProgramLanguages/ControlWork1/Program.cs
+++ ProgramLanguages/ControlWork1/Program.cs
@@ -7,4 +7,23 @@
 string[] arrayStart = new string[4] { "says", "257", "hi23", "1223"}; // Задаем изначальный массив размерностью 4 строки
 string[] arrayFinish = new string[arrayStart.Length]; // Задаем массив, соответствующий ТЗ
 int symbolsOfString = 3; // Задаем размер строки, соответствующий ТЗ
+void ArrayOfStrings(string[] arrayStart, string[] arrayFinish) // Задаем функцию поиска строк, соответствующих ТЗ
+{
+    int index = 0; // Задаем индекс строки, которую проверяем
+    for (int i = 0; i < arrayStart.Length;i++) // Задаем оператор итерации
+    {
+        if (arrayStart[i].Length <= symbolsOfString) // Задаем условие проверки строки на соответствие ТЗ
+        {
+            arrayFinish[index] = arrayStart[i]; // Записываем строку, соответствующую ТЗ в массив
+        }  
+    }  
+}
 
+void PrintArray(string[] array) // Задаем функцию вывода массива на печать
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
